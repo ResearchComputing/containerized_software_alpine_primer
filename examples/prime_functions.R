fba5cafda50cade70f0a3547702a90baaf1c76dd
monaghaa <33913248+monaghaa@users.noreply.github.com>
2024-02-27 09:24:36 -0700

    Add files via upload

---
+++ examples/prime_functions.R
@@ -0,0 +1,18 @@
+
+#Function to compute all prime numbers < argument "n"
+#https://www.r-bloggers.com/lets-be-faster-and-more-parallel-in-r-with-doparallel-package/
+getPrimeNumbers <- function(n) {  
+  n <- as.integer(n)
+  if(n > 1e6) stop("n too large")
+  primes <- rep(TRUE, n)
+  primes[1] <- FALSE
+  last.prime <- 2L
+  for (i in last.prime:floor(sqrt(n))) {
+    primes[seq.int(2L*last.prime, n, last.prime)] <- FALSE
+    last.prime <- last.prime + min(which(primes[(last.prime+1):n]))
+    }
+  which(primes)
+}
+
+#start R code below here
+getPrimeNumbers(100)
